ac7eadc6f04d0713d69201403a4544c5b725e54e
Surya170 <suryanarayana13271@gmail.com>
2022-08-28 22:58:54 +0530

    Main / Client App With Hibernate Persistance logic.

--- HBProj01-SaveObject/src/main/java/com/surya/test/SaveObjectTest.java
+++ HBProj01-SaveObject/src/main/java/com/surya/test/SaveObjectTest.java
@@ -3,6 +3,13 @@ package com.surya.test;
 public class SaveObjectTest {
 
 	public static void main(String[] args) {
+		
+		//Creating Configuration Object
+		
+		
+		//Creating SessionFactory Object
+		
+		//Creating Session Object
         
 	}
 
