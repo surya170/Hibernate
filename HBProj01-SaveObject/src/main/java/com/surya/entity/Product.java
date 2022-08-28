b943ffff423872568da835f84553ab6812fcf632
Surya170 <suryanarayana13271@gmail.com>
2022-08-28 22:49:50 +0530

    HBProj01

---
+++ HBProj01-SaveObject/src/main/java/com/surya/entity/Product.java
@@ -0,0 +1,43 @@
+// Entity class or BO Class or Model Class  or Persistence class
+package com.surya.entity;
+
+public class Product {
+	
+	private Integer pid;
+	private String pname;
+	private Float price;
+	private Float qty;
+	
+	// Getter and Setters 	
+	public Integer getPid() {
+		return pid;
+	}
+	public void setPid(Integer pid) {
+		this.pid = pid;
+	}
+	public String getPname() {
+		return pname;
+	}
+	public void setPname(String pname) {
+		this.pname = pname;
+	}
+	public Float getPrice() {
+		return price;
+	}
+	public void setPrice(Float price) {
+		this.price = price;
+	}
+	public Float getQty() {
+		return qty;
+	}
+	public void setQty(Float qty) {
+		this.qty = qty;
+	}
+	
+	// toString method.
+	@Override
+	public String toString() {
+		return "Product [pid=" + pid + ", pname=" + pname + ", price=" + price + ", qty=" + qty + "]";
+	}
+	
+}
---
+++ HBProj01-SaveObject/src/main/java/com/surya/test/SaveObjectTest.java
@@ -0,0 +1,9 @@
+package com.surya.test;
+
+public class SaveObjectTest {
+
+	public static void main(String[] args) {
+        
+	}
+
+}
